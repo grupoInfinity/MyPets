b7cae02040e25c05ce4ba4ed936a26db97cf6df5
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-01-18 10:51:43 -0600

    C

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -2,24 +2,71 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class dashboard extends StatelessWidget {
-  final String usr;
+  final int idUs;
 
-  dashboard({required this.usr});
-
+  dashboard({required this.idUs});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 20.0),
-              Text('Bienvenido '+usr),
-            ],
+      home: MyHomePage(id: idUs,),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final int id;
+
+  MyHomePage({required this.id});
+  @override
+  _MyHomePageState createState() => _MyHomePageState(idUs:id);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int idUs;
+  _MyHomePageState({required this.idUs});
+
+  late List<Widget> _children;
+  @override
+  void initState() {
+    super.initState();
+    // Inicializar _screens en initState para evitar problemas con StatefulWidget
+    _children = [
+      //ScreenOne(idUs: idUs),
+      //ScreenTwo(),
+      //ScreenThree(),
+    ];
+  }
+
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configuración',
+          ),
+        ],
       ),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
