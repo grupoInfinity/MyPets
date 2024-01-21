8391a4cbead39a153cf36f46bbbb5b4a334b0642
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-01-21 17:43:29 -0600

    C

--- lib/dashbo/second/infomasc.dart
+++ lib/dashbo/second/infomasc.dart
@@ -3,54 +3,55 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../contanst/app_contanst.dart';
 
 class infomasc extends StatelessWidget {
   final VoidCallback onClose;
   final String code;
-  infomasc({required this.onClose,required this.code});
+
+  infomasc({required this.onClose, required this.code});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              /*QrImageView(
-                data: 'FORTNITE ',
-                version: QrVersions.auto,
-                size: 200.0,
-              ),*/
-              SizedBox(height: 20.0),
-              Text('Bienvenido $code'),
-              ElevatedButton(
-                onPressed: onClose,
-                child: Text('CLICK DE VUELTA'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return _TuPantalla(code: code);
   }
-  Future<void> searchM(String code) async {
+}
+
+class _TuPantalla extends StatefulWidget {
+  final String code;
+  _TuPantalla({required this.code});
+
+  @override
+  _TuPantallaState createState() => _TuPantallaState();
+}
+
+class _TuPantallaState extends State<_TuPantalla> {
+  Mascota mascota =Mascota();
+  Vacuna vacuna = Vacuna();
+  @override
+  void initState() {
+    super.initState();
+    cargarDatos();
+  }
+
+  Future<void> cargarDatos() async {
     try {
       final url =
-          'http://192.168.1.11/MyPets_Admin/servicios/prc/prc_mascota.php?accion=C&codigo=$code';
+          'http://192.168.1.11/MyPets_Admin/servicios/prc/prc_mascota.php?accion=C&codigo=${widget.code}';
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        Map<String, dynamic> masc = json.decode(response.body);
-        if (masc['status'] == 1) {
-          /*Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => infomasc(onClose: onClose, code: code)),
-          );*/
+        Map<String, dynamic> mascData = json.decode(response.body);
+        if (mascData['status'] == 1) {
+          setState(() {
+            mascota = Mascota.fromJson(mascData['info'][0]['mascota']);
+            vacuna = Vacuna.fromJson(mascData['info'][0]['vacuna']);
+          });
         } else {
           //alerta(context, "Codigo no valido o inactivo");
         }
-      }
-      else {
+      } else {
         Fluttertoast.showToast(
           msg: "Error en la respuesta: ${response.statusCode}",
           toastLength: Toast.LENGTH_SHORT,
@@ -61,4 +62,142 @@ class infomasc extends StatelessWidget {
       print("Error: $e");
     }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            stops: [0.0, 1.0],
+            colors: [
+              Color.fromRGBO(18, 69, 140, 1.0),
+              Color.fromRGBO(110, 130, 158, 1.0),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(TSizes.defaultspace),
+            child: Column(
+              children: [
+                Container(
+                  height: 300,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('android/assets/images/Logo3.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                SizedBox(height: AppBar().preferredSize.height),
+                Text(
+                  "Ingrese el PIN",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: TSizes.spacebtwSections),
+                Form(
+                  child: Column(
+                    children: [
+                      Text('Nombre de la mascota: ${mascota.nmasc}'),
+                      Text('Nombre de la vacuna: ${vacuna.nombrevacuna}'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Mascota {
+  String? idmasc;
+  String? idtpmasc;
+  String? idmuni;
+  String? iddepto;
+  String? depto;
+  String? muni;
+  String? dueno;
+  String? mail;
+  String? telefono;
+  String? nmasc;
+  String? tipomasc;
+  String? direccion;
+  String? estadodir;
+  String? nacim;
+  String? foto;
+  String? codigo;
+  String? estado;
+  Mascota({
+    this.idmasc,
+    this.idtpmasc,
+    this.idmuni,
+    this.iddepto,
+    this.depto,
+    this.muni,
+    this.dueno,
+    this.mail,
+    this.telefono,
+    this.nmasc,
+    this.tipomasc,
+    this.direccion,
+    this.estadodir,
+    this.nacim,
+    this.foto,
+    this.codigo,
+    this.estado,
+  });
+
+  Mascota.fromJson(Map<String?, dynamic> json)
+      : idmasc = json['idmasc'],
+        idtpmasc = json['idtpmasc'],
+        idmuni = json['idmuni'],
+        iddepto = json['iddepto'],
+        depto = json['depto'],
+        muni = json['muni'],
+        dueno = json['dueno'],
+        mail = json['mail'],
+        telefono = json['telefono'],
+        nmasc = json['nmasc'],
+        tipomasc = json['tipomasc'],
+        direccion = json['direccion'],
+        estadodir = json['estadodir'],
+        nacim = json['nacim'],
+        foto = json['foto'],
+        codigo = json['codigo'],
+        estado = json['estado'];
+}
+
+class Vacuna {
+  String? idvacuna;
+  String? idmascota;
+  String? idtipovacuna;
+  String? nombrevacuna;
+  String? fechaCreacion;
+  Vacuna({
+    this.idvacuna,
+    this.idmascota,
+    this.idtipovacuna,
+    this.nombrevacuna,
+    this.fechaCreacion,
+  });
+
+  Vacuna.fromJson(Map<String?, dynamic> json)
+      : idvacuna = json['idvacuna'],
+        idmascota = json['idmascota'],
+        idtipovacuna = json['idtipovacuna'],
+        nombrevacuna = json['nombrevacuna'],
+        fechaCreacion = json['fecha_creacion'];
 }
