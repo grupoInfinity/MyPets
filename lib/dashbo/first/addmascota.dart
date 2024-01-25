9a00d4f44757e4f6ae3e350f57436514fd3d24de
LUIS EDUARDO ALVARENGA CLAROS <20220298@ricaldone.edu.sv>
2024-01-25 12:20:50 -0600

    C

--- lib/dashbo/first/addmascota.dart
+++ lib/dashbo/first/addmascota.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:mypets_app/contanst/app_contanst.dart';
 import 'package:iconsax/iconsax.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
-class AddMascota extends StatelessWidget {
+class AddMascota extends StatefulWidget {
+  final VoidCallback onClose;
+  final String usr;
+  AddMascota({required this.onClose,required this.usr});
+
+  @override
+  sbpage createState() => sbpage();
+}
+
+class sbpage extends State<AddMascota> {
+
+  File? _image;
+  Future<void> _getImageFromCamera() async {
+    final pickedFile =await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = pickedFile != null ? File(pickedFile.path) : null;
+    });
+  }
+
+  Future<void> _getImageFromGallery() async {
+    final pickedFile =await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = pickedFile != null ? File(pickedFile.path) : null;
+    });
+  }
+
+  String _convertImageToBase64(File image) {
+    List<int> imageBytes = image.readAsBytesSync();
+    String base64Image = base64Encode(imageBytes);
+    return base64Image;
+  }
+  late List<Departamento> departamentos;
+  late List<Municipio> municipios;
+  int selectedDepartamentoId = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    loadDepartamentos();
+  }
+
+  //loadDepartamentos();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,20 +77,49 @@ class AddMascota extends StatelessWidget {
               children: [
                 SizedBox(height: AppBar().preferredSize.height),
                 Center(
-  child: Text(
-    "Crea un nuevo perfil para tu mascota",
-    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-          color: Colors.white,
-        ),
-  ),
-),
+                  child: Text(
+                    "Crea un nuevo perfil para tu mascota ${widget.usr}",
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ),
                 const SizedBox(height: TSizes.spacebtwSections),
                 Form(
                   child: Column(
                     children: [
+                      DropdownButton<int>(
+                        value: selectedDepartamentoId,
+                        items: departamentos.map((departamento) {
+                          return DropdownMenuItem<int>(
+                            value: departamento.id,
+                            child: Text(departamento.nombre),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedDepartamentoId = value!;
+                            loadMunicipios(selectedDepartamentoId);
+                          });
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      DropdownButton<String>(
+                        value: '',
+                        items: municipios.map((municipio) {
+                          return DropdownMenuItem<String>(
+                            value: municipio.nombre,
+                            child: Text(municipio.nombre),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          // Hacer algo con el municipio seleccionado
+                          print('Municipio seleccionado: $value');
+                        },
+                      ),
                       TextFormField(
                         decoration: const InputDecoration(
-                          labelText: "Campo 1",
+                          labelText: "Nombre de la mascota",
                           labelStyle: TextStyle(color: Colors.white),
                           focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.black),
@@ -95,7 +170,8 @@ class AddMascota extends StatelessWidget {
                           focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.black),
                           ),
-                          prefixIcon: Icon(Iconsax.activity, color: Colors.white),
+                          prefixIcon:
+                              Icon(Iconsax.activity, color: Colors.white),
                         ),
                       ),
                       const SizedBox(height: TSizes.spacebtwInputFields),
@@ -107,8 +183,8 @@ class AddMascota extends StatelessWidget {
                             borderSide: BorderSide(color: Colors.black),
                           ),
                           labelStyle: TextStyle(color: Colors.white),
-                          prefixIcon: Icon(Iconsax.password_check, color: Colors.white),
-                        
+                          prefixIcon:
+                              Icon(Iconsax.password_check, color: Colors.white),
                         ),
                       ),
                       const SizedBox(height: 30),
@@ -120,10 +196,7 @@ class AddMascota extends StatelessWidget {
                             onPrimary: Colors.white,
                             fixedSize: Size(0, 50),
                           ),
-                          onPressed: () {
-                            
-                           
-                          },
+                          onPressed: () {},
                           child: Text("Agregar Mascota"),
                         ),
                       ),
@@ -136,5 +209,74 @@ class AddMascota extends StatelessWidget {
         ),
       ),
     );
+  }
+  Future<void> loadDepartamentos() async {
+    try {
+      departamentos = await getDepartamentos();
+      setState(() {
+        selectedDepartamentoId = departamentos[0].id;
+      });
+      loadMunicipios(selectedDepartamentoId);
+    } catch (e) {
+      print('Error loading departamentos: $e');
+    }
+  }
+
+  Future<void> loadMunicipios(int departamentoId) async {
+    try {
+      municipios = await getMunicipios(departamentoId);
+      setState(() {});
+    } catch (e) {
+      print('Error loading municipios: $e');
+    }
+  }
+  Future<List<Departamento>> getDepartamentos() async {
+    final response =
+    await http.get(Uri.parse('https://ginfinity.xyz/MyPets_Admin/servicios/ctg/ctg_depto.php?accion=C&estado=A'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => Departamento.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load departamentos');
+    }
+  }
+
+  Future<List<Municipio>> getMunicipios(int departamentoId) async {
+    final response = await
+    http.get(Uri.parse('https://ginfinity.xyz/MyPets_Admin/servicios/ctg/ctg_muni.php?accion=C&estado=A&idDepto=$departamentoId'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      return data.map((json) => Municipio.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load municipios');
+    }
+  }
+}
+
+
+class Departamento {
+  final int id;
+  final String nombre;
+
+  Departamento({required this.id, required this.nombre});
+
+  // Método para convertir un mapa a un objeto Departamento
+  factory Departamento.fromJson(Map<String, dynamic> json) {
+    return Departamento(id: json['id'], nombre: json['descripcion']);
+  }
+}
+
+class Municipio {
+  final int id;
+  final String nombre;
+  final int departamentoId;
+
+  Municipio({required this.id, required this.nombre, required this.departamentoId});
+
+  // Método para convertir un mapa a un objeto Municipio
+  factory Municipio.fromJson(Map<String, dynamic> json) {
+    return Municipio(id: json['id']['id'], nombre: json['descripcion'], departamentoId: json['id']['id_depto']);
   }
 }
--- lib/dashbo/first/home.dart
+++ lib/dashbo/first/home.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:mypets_app/dashbo/first/addmascota.dart';
 
 class home extends StatefulWidget {
-
   final String usr;
-
 
   home({required this.usr});
 
@@ -16,6 +14,7 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   int _current = 0;
   dynamic _selectedIndex = {};
+  bool subpage = false;
 
   CarouselController _carouselController = new CarouselController();
 
@@ -30,20 +29,18 @@ class _homeState extends State<home> {
       'image': 'android/assets/images/Mascota2.png',
       'description': 'Perro'
     },
-    
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
- onPressed: () {
-Navigator.push(context, MaterialPageRoute(builder:(context) => AddMascota()),
-);
-                                  },
-  backgroundColor: const Color.fromARGB(255, 51, 163, 255),
-  child: Icon(Icons.add),
-),
+        onPressed: () {
+
+        },
+        backgroundColor: const Color.fromARGB(255, 51, 163, 255),
+        child: Icon(Icons.add),
+      ),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -55,84 +52,91 @@ Navigator.push(context, MaterialPageRoute(builder:(context) => AddMascota()),
         width: double.infinity,
         height: double.infinity,
         child: CarouselSlider(
-          carouselController: _carouselController,
-          options: CarouselOptions(
-            height: 430.0,
-            aspectRatio: 16/9,
-            viewportFraction: 0.70,
-            enlargeCenterPage: true,
-            pageSnapping: true,
-            onPageChanged: (index, reason) {
-              setState(() {
-                _current = index;
-              });
-            }
-          ),
-          items: _products.map((movie) {
-            return Builder(
-              builder: (BuildContext context) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (_selectedIndex == movie) {
-                        _selectedIndex = {};
-                      } else {
-                        _selectedIndex = movie;
-                      }
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 300),
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      border: _selectedIndex == movie ? Border.all(color: Colors.blue.shade500, width: 3) : null,
-                      boxShadow: _selectedIndex == movie ? [
-                        BoxShadow(
-                          color: Colors.blue.shade100,
-                          blurRadius: 30,
-                          offset: Offset(0, 10)
-                        )
-                      ] : [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          blurRadius: 20,
-                          offset: Offset(0, 5)
-                        )
-                      ]
-                    ),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 320,
-                            margin: EdgeInsets.only(top: 10),
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
+            carouselController: _carouselController,
+            options: CarouselOptions(
+                height: 430.0,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.70,
+                enlargeCenterPage: true,
+                pageSnapping: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+            items: _products.map((movie) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (_selectedIndex == movie) {
+                          _selectedIndex = {};
+                        } else {
+                          _selectedIndex = movie;
+                        }
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                          border: _selectedIndex == movie
+                              ? Border.all(
+                                  color: Colors.blue.shade500, width: 3)
+                              : null,
+                          boxShadow: _selectedIndex == movie
+                              ? [
+                                  BoxShadow(
+                                      color: Colors.blue.shade100,
+                                      blurRadius: 30,
+                                      offset: Offset(0, 10))
+                                ]
+                              : [
+                                  BoxShadow(
+                                      color: Colors.grey.withOpacity(0.2),
+                                      blurRadius: 20,
+                                      offset: Offset(0, 5))
+                                ]),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 320,
+                              margin: EdgeInsets.only(top: 10),
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Image.asset(_products[0]['image'],
+                                  fit: BoxFit.cover),
                             ),
-                            child: Image.asset(_products[0]['image'], fit: BoxFit.cover),
-                          ),
-                          SizedBox(height: 20,),
-                          Text(movie['title'], style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),),
-                          SizedBox(height: 20,),
-                          Text(movie['description'], style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey.shade600
-                          ),),
-                        ],
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              movie['title'],
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              movie['description'],
+                              style: TextStyle(
+                                  fontSize: 14, color: Colors.grey.shade600),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-            );
-          }).toList()
-        ),
+                  );
+                },
+              );
+            }).toList()),
       ),
     );
   }
