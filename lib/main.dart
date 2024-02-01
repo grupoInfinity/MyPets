18e2abf6c58a6c36377d43e8a41a59893c67fef8
Fernandouu <20190369@ricaldone.edu.sv>
2024-02-01 11:28:39 -0600

    C

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mypets_app/Splash.dart';
-import 'package:mypets_app/dashbo/first/addmascota.dart';
 import 'package:mypets_app/login.dart';
 import 'package:mypets_app/principal_screen.dart';
 import 'package:mypets_app/registro.dart';
